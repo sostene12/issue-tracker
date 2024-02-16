e52ed613992e6ef31475a48482a02841958b825f
sostene12 <ngarukiyimanasostene@gmail.com>
2024-02-16 21:12:07 +0200

    implemented form validation, error message components , spin and new issuer api integration

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,63 @@
-'use client';
-import { Button, TextField } from '@radix-ui/themes'
+"use client";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import { useForm, Controller, set } from "react-hook-form";
+import axios from "axios";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from "@/app/ValidationSchemas";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root>
-            <TextField.Input placeholder='Title' />
-        </TextField.Root>
-        <SimpleMDE placeholder='Description' />
-        <Button>Submit New Issue</Button>
-    </div>
-  )
-}
+  const router = useRouter();
+  const { register, control, handleSubmit,formState:{ errors } } = useForm<IssueForm>({
+    resolver:zodResolver(createIssueSchema )
+  });
+  const [error, setError] = useState("");
+  const [isSubmiting,setIsSubmiting] = useState(false);
 
-export default NewIssuePage
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            setIsSubmiting(true);
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setIsSubmiting(false);
+            setError("An error occurred. Please try again.");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button disabled={isSubmiting}>Submit New Issue { isSubmiting && <Spinner />} </Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
