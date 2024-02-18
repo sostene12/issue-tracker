850d73ce055098498501f6e945dac4054ccd4145
sostene12 <ngarukiyimanasostene@gmail.com>
2024-02-18 18:29:10 +0200

    add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import prisma from '@/prisma/client';
-import { notFound } from 'next/navigation';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import ReactMarkdown from "react-markdown";
 
-interface Props{
-    params: { id: string }
+interface Props {
+  params: { id: string };
 }
 
-const IssueDetailsPage = async ({ params:{ id } }:Props) => {
-   const issue = await prisma.issue.findUnique({where: {id:parseInt(id)}});
-   if(!issue)
-    notFound();
+const IssueDetailsPage = async ({ params: { id } }: Props) => {
+  const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
+  if (!issue) notFound();
   return (
     <div>
-        <Heading>{issue.title}</Heading>
-        <Flex gap='4' my='2'>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="4" my="2">
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card>
-        <p>{issue.description}</p>
-        </Card>
+      </Flex>
+      <Card className="prose" mt='4'>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default IssueDetailsPage
+export default IssueDetailsPage;
