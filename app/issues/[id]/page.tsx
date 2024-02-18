292fb1b0426e03358679c9c1d6622f934e62d9e4
sostene12 <ngarukiyimanasostene@gmail.com>
2024-02-18 19:47:48 +0200

    Refactor: apply SRP(Single Responsibility Principle)

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,8 @@
-import ReactMarkdown from "react-markdown";
 import { notFound } from "next/navigation";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Grid} from "@radix-ui/themes";
 import prisma from "@/prisma/client";
-import { IssueStatusBadge } from "@/app/components";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: { id: string };
@@ -12,16 +12,14 @@ const IssueDetailsPage = async ({ params: { id } }: Props) => {
   const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
   if (!issue) notFound();
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="4" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt='4'>
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <IssueDetails issue={issue} />
+      </Box>
+      <Box>
+        <EditIssueButton issueId={issue.id} />
+      </Box>
+    </Grid>
   );
 };
 
