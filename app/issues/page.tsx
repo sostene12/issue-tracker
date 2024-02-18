c36b8d3c9d3d55a5719dfc854cb3e351e699e27c
sostene12 <ngarukiyimanasostene@gmail.com>
2024-02-18 18:44:04 +0200

    biild a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Table } from "@radix-ui/themes";
+import { Table} from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import delay from "delay";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssuesActions from "./IssuesActions";
-import Link from "next/link";
+import Link from "../components/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
