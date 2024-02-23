ae3fb7604ac0c1c0ed03a7e157ff77cce30697b6
sostene12 <ngarukiyimanasostene@gmail.com>
2024-02-23 22:19:01 +0200

    add the prisma adapter

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Container, Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -24,6 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <AuthProvider>
       <body className={inter.variable}>
         <Theme appearance="light" accentColor="violet">
           <NavBar />
@@ -32,6 +34,7 @@ export default function RootLayout({
           </main>
         </Theme>
       </body>
+      </AuthProvider>
     </html>
   );
 }
