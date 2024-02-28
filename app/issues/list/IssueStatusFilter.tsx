ecddb9d23bf9e968ee42363fa10985fc21d1705f
sostene12 <ngarukiyimanasostene@gmail.com>
2024-02-28 16:03:07 +0200

    fix filtering bugs

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value?: Status }[] = [
@@ -13,15 +13,22 @@ const statuses: { label: string; value?: Status }[] = [
 ];
 
 const IssueStatusFilter = () => {
-
-    const router =  useRouter();
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
-    <Select.Root onValueChange={(status) => {
-        const query =  status ? `?status=${status}` : "";
-        router.push(`/issues/list${query}`);
-    } }>
-      <Select.Trigger   aria-placeholder="Filter by Status..." />
+    <Select.Root
+    defaultValue={searchParams.get("status") || ""}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+        const query = params.size ? "?" + params.toString() : "";
+        router.push("/issues/list" + query);
+      }}
+    > 
+      <Select.Trigger aria-placeholder="Filter by Status..." />
       <Select.Content>
         {statuses.map((status) => (
           <Select.Item key={status.value} value={status.value || ""}>
