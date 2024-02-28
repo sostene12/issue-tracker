af9b1f7cd368e4956d5ddc854556fe306b749b6b
sostene12 <ngarukiyimanasostene@gmail.com>
2024-02-28 15:40:11 +0200

    make collumns sortable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,12 +1,15 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
+import NextLink from "next/link";
 import { IssueStatusBadge, Link } from "@/app/components";
 import IssuesActions from "./IssuesActions";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Props {
   searchParams: {
     status: Status;
+    orderBy: keyof Issue;
   };
 }
 
@@ -15,21 +18,27 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const colluns: { label: string; value: keyof Issue; className?: string }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
+
   const issues = await prisma.issue.findMany({ where: { status } });
   return (
-    <div> 
+    <div>
       <IssuesActions />
 
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {colluns.map((col) => (
+              <Table.ColumnHeaderCell key={col.value} className={col.className}>
+                <NextLink href={{query:{...searchParams,orderBy:col.value}}}>{col.label}</NextLink>
+                {col.value === searchParams.orderBy &&  <ArrowUpIcon className="inline" />}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
