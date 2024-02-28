81c1767caa46c48a70c83951868fb2c4ecdec987
sostene12 <ngarukiyimanasostene@gmail.com>
2024-02-28 13:16:08 +0200

    refactor AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,53 +1,57 @@
 "use client";
-import axios from "axios";
-import toast,{Toaster} from "react-hot-toast";
+import Skeleton from "@/app/components/Skeleton";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
-import Skeleton from '@/app/components/Skeleton';
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
-const AssigneeSelect = ({issue}:{issue:Issue}) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 100, //60 seconds
-    retry:3 
-  });
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const { data: users, error, isLoading } = useUsers();
 
-  if(isLoading) return <Skeleton />
+  if (isLoading) return <Skeleton />;
 
-  if(error) return null;
-
+  if (error) return null;
+  const assignIssue = async (userId: string) => {
+    try {
+      await axios.patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId || null,
+      });
+    } catch (error) {
+      toast.error("Changes could not be saved.");
+    }
+  };
 
   return (
     <>
-    <Select.Root defaultValue={issue.assignedToUserId || ""} onValueChange={ async (userId) => {
-      try {
-        await axios.patch(`/api/issues/${issue.id}`, { assignedToUserId: userId || null });
-      } catch (error) {
-        toast.error('Changes could not be saved.')
-      }
-    }  }>
-      <Select.Trigger aria-placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
-    <Toaster />
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={assignIssue}
+      >
+        <Select.Trigger aria-placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
     </>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 100, //60 seconds
+    retry: 3,
+  });
 
 export default AssigneeSelect;
