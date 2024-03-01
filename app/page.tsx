e7fcf06bb9dc27bdeb208407f377a143ed718207
sostene12 <ngarukiyimanasostene@gmail.com>
2024-03-01 10:41:58 +0200

    Build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import NavBar from "./NavBar"
+import Pagination from "./components/Pagination"
 
 export default function Home() {
   return (
     <div>
       <h3>Hello world!</h3>
+      <Pagination itemCount={100} pageSize={10} currentPage={10} />
     </div>
   )
 }
